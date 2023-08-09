bfbdbc4f6137810f818a46cf89cbefd21586b2b3
oh seung hyun <kjb1045@naver.com>
2023-08-09 16:36:32 +0900

    movie2023_test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import axios from 'axios';
 import React from 'react';
+import Movie from './Movie'; 
+import './style/App.css';
+
 class App extends React.Component {
   // react.component의 기능들을 App가 상속받아 사용 할 수 있다.
 
-  state ={
+  state = {
     isLoading: true,
-    movie:[], // YTS API를 연동하여 작성 예정
-  }
-  componentDidMount(){
-    // setTimeout(() => {
-    //   this.setState({isLoading:false});
-    // }, 6000);
-    // axios는 릭액트에서 API를 연동하는 라이브러리이다.
-    // axios.get() 메서드는 값을 출력할 때 사용한다.
-    axios.get('http://yts-proxy.now.sh/list_movies.json');
+    movies: [], // YTS API를 연동하여 작성 예정
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
+    );
+
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
   }
   render() {
-     const {isLoading} = this.state;
+    const { isLoading, movies } = this.state;
     return (
-    <>
-      {isLoading?'Loading...':'We are ready'};
-    </>
+      <section className='container'>
+        {isLoading
+          ? (<div className='loader'><span className='loader_text'>Loading...</span></div>)
+          : ( 
+           <div className='movies'>
+            {movies.map( movie => (
+                <Movie 
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+             ))}
+          </div> 
+         )} 
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,37 @@
-function Movie({ title, poster }) {
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style/Movie.css';
+
+function Movie({ title, year, summary, poster, genres }) {
   return (
-    <div>
-      <h1>i like {title}</h1>
-      <img src={poster} />
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="movie_genres">
+          {genres.map((genre, index) => {
+            return (
+              <li key={index} className="movie_genre">
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
+        <p className="movie_summary">{summary.slice(0, 180)}</p>
+      </div>
     </div>
   );
 }
+
+// propTypes 입력시 p는 반드시 소문자로 입력
+Movie.propTypes = {
+  // id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Movie;
