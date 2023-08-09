46b7f64126a73bd3120bcf510be14ed35ee13e34
oh seung hyun <kjb1045@naver.com>
2023-08-09 22:43:55 +0900

    movieList_2023

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React from 'react';
-import Movie from './Movie'; 
+import Movie from './Movie';
 import './style/App.css';
 
 class App extends React.Component {
@@ -10,30 +10,39 @@ class App extends React.Component {
     isLoading: true,
     movies: [], // YTS API를 연동하여 작성 예정
   };
+
+  // async(비동기) getMovies()가 비동기이니 좀 기다려 달라고 자바스크립트에게 명령 함
+  // await는 axios.get()이 실행이 끝날때까지 기다렸다가 다음을 실행해 달라는 명령어 임
+  // 자바스크립트가 단일스레드로 동기식방식이므로 비동기식으로 변경해주는 작업 임
   getMovies = async () => {
     const {
       data: {
         data: { movies },
       },
     } = await axios.get(
-      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
+      'https://yts.mx/api/v2/list_movies.json?sort_by=rating'
     );
-
     this.setState({ movies, isLoading: false });
   };
+
   componentDidMount() {
     this.getMovies();
   }
+
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section className='container'>
-        {isLoading
-          ? (<div className='loader'><span className='loader_text'>Loading...</span></div>)
-          : ( 
-           <div className='movies'>
-            {movies.map( movie => (
-                <Movie 
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
+              // console.log(movie);
+              return (
+                <Movie
                   key={movie.id}
                   id={movie.id}
                   year={movie.year}
@@ -42,9 +51,10 @@ class App extends React.Component {
                   poster={movie.medium_cover_image}
                   genres={movie.genres}
                 />
-             ))}
-          </div> 
-         )} 
+              );
+            })}
+          </div>
+        )}
       </section>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -2,36 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style/Movie.css';
 
-function Movie({ title, year, summary, poster, genres }) {
+function Movie({ year, title, summary, poster,genres }) {
   return (
-    <div className="movie">
+   <section>
+    <div className="container">
       <img src={poster} alt={title} title={title} />
       <div className="movie_data">
         <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="movie_genres">
-          {genres.map((genre, index) => {
-            return (
-              <li key={index} className="movie_genre">
-                {genre}
-              </li>
-            );
-          })}
+        <h4 className="movie_year">{year}</h4>
+        <ul className='movie_genres'>
+          { genres.map((genre,index)=>{
+             return (
+              <li key={index} className='movie_genre'>{genre}</li>
+             );
+          })
+          }
         </ul>
-        <p className="movie_summary">{summary.slice(0, 180)}</p>
+        <p className="movie_summary">{summary.slice(0,180)}</p>
       </div>
     </div>
+  </section>  
   );
 }
 
-// propTypes 입력시 p는 반드시 소문자로 입력
 Movie.propTypes = {
-  // id: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  genres:PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
